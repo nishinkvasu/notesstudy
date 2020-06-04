f3e2765920725b64c362c558ea7a49079e01b91a
Nishin K Vasu <Nishin.Karippadath.Vasu@continental-corporation.com>
2020-06-04 08:31:47 +0800

    Added parsing lines

--- ud_nd_cpp/codesnip/board.cpp
+++ ud_nd_cpp/codesnip/board.cpp
@@ -1,27 +1,39 @@
+#include <fstream>
+#include <sstream>
 #include <iostream>
 #include <string>
 #include <vector>
-#include <fstream>
 using std::cout;
+using std::ifstream;
 using std::string;
 using std::vector;
-using std::ifstream;
+using std::istringstream;
 
-// TODO: Add the ReadBoardFile function here.
-void ReadBoardFile(void)
-{
-  ifstream iFile;
-  iFile.open("1.board");
+// TODO: Add the ParseLine function here.
+vector<int> ParseLine(string s) {
+  istringstream mystream(s);
+  int n;
+  char c;
+  vector<int> iList;
   
-  if(iFile)
-  {
-    cout << "File is successfully opened" <<"\n";
-    string sLine;
-    while(getline(iFile, sLine))
-      cout << sLine << "\n";
+  // traverse through the string
+  while(mystream >> n >> c)
+  {    
+    //cout << n << " " << c;
+    iList.push_back(n);
+  }
+	return iList;
+}
+void ReadBoardFile(string path) {
+  ifstream myfile (path);
+  if (myfile) {
+    string line;
+    while (getline(myfile, line)) {
+      cout << line << "\n";
+    }
   }
 }
-// PrintBoard not used in this exercise
+
 void PrintBoard(const vector<vector<int>> board) {
   for (int i = 0; i < board.size(); i++) {
     for (int j = 0; j < board[i].size(); j++) {
@@ -31,9 +43,11 @@ void PrintBoard(const vector<vector<int>> board) {
   }
 }
 
+#include "test.cpp" // For testing.
+
 int main() {
-  // TODO: Call the ReadBoardFile function here.
-  ReadBoardFile();
-  // Leave the following line commented out.
-  //PrintBoard(board);
+  ReadBoardFile("1.board");
+  TestParseLine(); // For testing.
+  // Leave commented out.
+  // PrintBoard(board);
 }
