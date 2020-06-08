7f94b3f291d205f590c998b3ec6174424775e1e5
Nishin K Vasu <Nishin.Karippadath.Vasu@continental-corporation.com>
2020-06-08 09:12:57 +0800

    Adding initial code for A* search

--- ud_nd_cpp/codesnip/board.cpp
+++ ud_nd_cpp/codesnip/board.cpp
@@ -1,58 +1,96 @@
 #include <fstream>
 #include <iostream>
-#include <string>
 #include <sstream>
+#include <string>
 #include <vector>
+using std::abs;
 using std::cout;
 using std::ifstream;
 using std::istringstream;
 using std::string;
 using std::vector;
 
+enum class State { kEmpty, kObstacle, kClosed };
 
-vector<int> ParseLine(string line) {
-    istringstream sline(line);
-    int n;
-    char c;
-    vector<int> row;
-    while (sline >> n >> c && c == ',') {
-      row.push_back(n);
+vector<State> ParseLine(string line) {
+  istringstream sline(line);
+  int n;
+  char c;
+  vector<State> row;
+  while (sline >> n >> c && c == ',') {
+    if (n == 0) {
+      row.push_back(State::kEmpty);
+    } else {
+      row.push_back(State::kObstacle);
     }
-    return row;
+  }
+  return row;
 }
 
-// TODO: Change the return type of ReadBoardFile.
-vector<vector<int>> ReadBoardFile(string path) {
-  ifstream myfile (path);
-  // TODO: Declare an empty board variable here with
-  // type vector<vector<int>>.
-  vector<vector<int>> board;
+vector<vector<State>> ReadBoardFile(string path) {
+  ifstream myfile(path);
+  vector<vector<State>> board{};
   if (myfile) {
     string line;
     while (getline(myfile, line)) {
-      // TODO: Replace the "cout" code with a call to ParseLine for each line and push the results of ParseLine to the back of the board.
-      board.push_back(ParseLine(line));
-      //cout << line << "\n";
+      vector<State> row = ParseLine(line);
+      board.push_back(row);
     }
   }
   return board;
-  // TODO: Return the board variable.
 }
 
-void PrintBoard(const vector<vector<int>> board) {
+// Calculate the manhattan distance
+int Heuristic(int x1, int y1, int x2, int y2) {
+  return abs(x2 - x1) + abs(y2 - y1);
+}
+
+// TODO: Write the AddToOpen function here.
+void AddToOpen(int x, int y, int g, int h,
+               vector<vector<int>> &openNodes, vector<vector<State>> &grid) {
+  vector<int> node = {x, y, g, h};
+  openNodes.push_back(node);
+  grid[x][y] = State::kClosed;
+}
+
+/**
+ * Implementation of A* search algorithm
+ */
+vector<vector<State>> Search(vector<vector<State>> grid, int init[2],
+                             int goal[2]) {
+
+  cout << "No path found!"
+       << "\n";
+  return std::vector<vector<State>>{};
+}
+
+string CellString(State cell) {
+  switch (cell) {
+  case State::kObstacle:
+    return "⛰️   ";
+  default:
+    return "0   ";
+  }
+}
+
+void PrintBoard(const vector<vector<State>> board) {
   for (int i = 0; i < board.size(); i++) {
     for (int j = 0; j < board[i].size(); j++) {
-      cout << board[i][j];
+      cout << CellString(board[i][j]);
     }
     cout << "\n";
   }
 }
 
+//#include "test.cpp"
+
 int main() {
-   
-  // TODO: Store the output of ReadBoardFile in the "board" variable.
-  vector<vector<int>> board;
-  board = ReadBoardFile("1.board");
-  // TODO: Uncomment PrintBoard below to print "board".
-  PrintBoard(board);
+  int init[2]{0, 0};
+  int goal[2]{4, 5};
+  auto board = ReadBoardFile("1.board");
+  auto solution = Search(board, init, goal);
+  PrintBoard(solution);
+  // Tests
+  //TestHeuristic();
+  //TestAddToOpen();
 }
